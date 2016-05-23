3a68afb90a7976276a891e18075d0f7d15ab08c7
Dilu9218 <dilu9218@gmail.com>
2016-05-23 18:20:22 +0530

    Add files via upload

---
+++ EllipseDilushi.m
@@ -0,0 +1,29 @@
+%#!/usr/bin/octave -qf
+
+ #{x1=1;
+x2=4;
+y1=3;
+y2=7;
+
+
+ a = 1/2*sqrt((x2-x1)^2+(y2-y1)^2);
+ b = a*sqrt(1-e^2);
+ t = linspace(0,2*pi);
+ X = a*cos(t);
+ Y = b*sin(t);
+ w = atan2(y2-y1,x2-x1);
+ x = (x1+x2)/2 + X*cos(w) - Y*sin(w);
+ y = (y1+y2)/2 + X*sin(w) + Y*cos(w);
+#}
+%plot(x,y)
+%axis equal
+
+a=5; % horizontal radius
+b=10; % vertical radius
+x0=0; % x0,y0 ellipse centre coordinates
+y0=0;
+t=-pi:0.01:pi;
+x=x0+a*cos(t);
+y=y0+b*sin(t);
+plot(x,y)
+axis equal
